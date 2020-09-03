22d5eb820616d68ee3f3e92e69929d964646655d
Jackson Chen <jacksonchen@Jacksons-Air.home>
2020-09-02 23:06:32 -0400

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,9 +2,24 @@ import React from 'react';
 import { movies } from '../data';
 
 const Movies = () => {
+
+  function renderMovies() {
+    return movies.map(movie => (
+      <div>
+        <h3>Name: {movie.title}</h3>
+        <p>Time: {movie.time}</p>
+        <p>Genres:</p>
+        <ul>
+          {movie.genres.map(genre => (<li key={genre}>{genre}</li>))}
+        </ul>
+      </div>
+    ))
+  }
+
   return (
     <div>
-        {/*{code here}*/}
+        <h1>Movies Page</h1>
+        {renderMovies()}
     </div>
   );
 };
